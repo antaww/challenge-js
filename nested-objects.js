fc4bbec1f1fce08dab5bbf325daaecc3a550af04
antaww <anta.pizzetta@gmail.com>
2021-11-18 10:34:07 +0100

    nested

--- nested-objects.js
+++ nested-objects.js
@@ -1,10 +1,11 @@
+let street = {
+    name : 'name',
+    age : 1,
+}
+
 let address = {
     city : 'city',
     postcode  : 1,
     street
 }
 
-let street = {
-    name : 'name',
-    age : 1,
-}
