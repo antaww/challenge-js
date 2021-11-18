908b81732c24829bb13961a7721297039a86cba3
antaww <anta.pizzetta@gmail.com>
2021-11-18 10:30:14 +0100

    object

--- alter-ego.js
+++ alter-ego.js
@@ -1,3 +1,3 @@
 alterEgo.self = 'altered'
-alterEgo.fullName = `${alterEgo.name} ${alterEgo.lastName}`
+alterEgo.fullName = `${alterEgo.firstName} ${alterEgo.lastName}`
 alterEgo.points = alterEgo.points + 10
