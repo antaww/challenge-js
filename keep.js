7d3f8a27c865905f27d4a5be7ec2508f5deee3f2
antaww <anta.pizzetta@gmail.com>
2021-11-18 16:41:30 +0100

    keep

--- keep.js
+++ keep.js
@@ -1,4 +1,3 @@
 let keepFirst = alphabet.slice(0, 6);
 let keepLast = alphabet.slice(-7);
-let keepThreeFirst = alphabet.slice(0, 3);
-let keepFirstLast = alphabet.slice(keepThreeFirst, -3)
+let keepFirstLast = alphabet.slice(0, 3, -4)
