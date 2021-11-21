6085a09d32f4b4eff916e4bdc332745ca59d1f78
antaww <anta.pizzetta@gmail.com>
2021-11-21 14:56:06 +0100

    blacksmith

--- griswold-the-blacksmith.js
+++ griswold-the-blacksmith.js
@@ -1,14 +1,11 @@
-let purchasableGoods = ['arrows', 'boots', 'sword'];
+const purchasableGoods = ['arrows', 'boots', 'sword'];
 
 if(playerCoins<3){
     purchasableGoods.length = 0;
 }else if(playerCoins>=3){
-    purchasableGoods[0];
+    purchasableGoods.slice(0,1)
     if(playerCoins>=44){
-        purchasableGoods[0];
-        purchasableGoods[1];
+        purchasableGoods.slice(0,2)
         if(playerCoins>=299.99){
-            purchasableGoods[0];
-            purchasableGoods[1];
-            purchasableGoods[2];
+            purchasableGoods.slice(0,3)
 }}}
