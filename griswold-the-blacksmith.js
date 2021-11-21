7ff74d1b81309a4f9337c29474f2318c783b9a26
antaww <anta.pizzetta@gmail.com>
2021-11-21 12:04:10 +0100

    blacksmith

---
+++ griswold-the-blacksmith.js
@@ -0,0 +1,12 @@
+let purchasableGoods = ['arrows', 'boots', 'sword'];
+
+
+if(playerCoins<3){
+    purchasableGoods[null];
+}else if(playerCoins>=3){
+    purchasableGoods[0];
+} else if(playerCoins>=44){
+    purchasableGoods[0,1];
+} else if(playerCoins>=299.99){
+    purchasableGoods[0,1,2];
+}
