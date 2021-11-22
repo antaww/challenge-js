8d190dbaa8e1777ff6f89ec711c8e72e9bf9ee24
antaww <anta.pizzetta@gmail.com>
2021-11-22 20:25:50 +0100

    olympic

--- olympic.js
+++ olympic.js
@@ -1,9 +1,11 @@
-if(athletes.indexOf(1)){
-    console.log('🥇 Pedro is #1');
-} else if(athletes.indexOf(2)){
-    console.log('🥈 Pedro is #2');
-} else if(athletes.indexOf(3)){
-    console.log('🥉 Pedro is #3');
+if(athletes.indexOf('Pedro') == 1){
+    console.log('🥇');
+} else if(athletes.indexOf('Pedro') == 2){
+    console.log('🥈');
+} else if(athletes.indexOf('Pedro') == 3){
+    console.log('🥉');
 } else {
-    console.log('😞 Perdo is #' + athletes.indexOf());
+    console.log('😞');
 }
+
+console.log(' Perdo is #' + athletes.indexOf('Pedro'));
