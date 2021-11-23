8e904b23aec23ede4e7df3f9bf28ffd270d30dbc
antaww <anta.pizzetta@gmail.com>
2021-11-23 10:55:48 +0100

    two

---
+++ two-can-play-that-game.js
@@ -0,0 +1,10 @@
+let duos = (arg1, arg2) => {
+    console.log(arg1 + ' and ' + arg2 + ' !')
+}
+
+let duosWork = (arg1, arg2, arg3) => {
+    console.log(arg1 + ' and ' + arg2 + ' ' + arg3 + ' !');
+}
+
+
+
