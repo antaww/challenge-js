729d0bbc3a6047ce4fb085bd014212f84bab6116
antaww <anta.pizzetta@gmail.com>
2021-11-23 10:46:09 +0100

    purpose

--- what-9000.js
+++
@@ -1,4 +0,0 @@
-const powerlevel.name = 100;
-if(name == 'Goku'){
-    powerlevel.name = 10000;
-}
---
+++ what-is-my-purpose.js
@@ -0,0 +1,12 @@
+let ask = () => {
+    console.log('What is my purpose ?');
+}
+
+let reply = () => {
+    console.log('You pass butter.');
+}
+
+ask();
+reply();
+
+
